192dc6ee1750930c076f844138b30143852262ea
yluo <yluotx@gmail.com>
2016-08-03 12:23:44 +0800

    创建上下滚动的跑马灯

---
+++ src/com/yluo/yluomarqueeview/MainActivity.java
@@ -0,0 +1,25 @@
+package com.yluo.yluomarqueeview;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+
+public class MainActivity extends Activity {
+	
+	YluoMarqueeView yluo_mar;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		yluo_mar = (YluoMarqueeView) findViewById(R.id.yluo_mar);
+	}
+	
+	public void nextLine(View view) {
+		yluo_mar.startPlay();
+	}
+
+}
---
+++ src/com/yluo/yluomarqueeview/YluoMarqueeView.java
@@ -0,0 +1,238 @@
+package com.yluo.yluomarqueeview;
+
+import java.util.ArrayList;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.text.TextPaint;
+import android.text.TextUtils;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.widget.Scroller;
+
+public class YluoMarqueeView extends View {
+
+	private static final String TAG = "YluoMarqueeView";
+
+	private Paint mPaint;
+
+	private Rect mTexRect;
+
+	private ArrayList<MarqueeLine> mContents;
+
+	private int mLineHeight = 50;
+
+	private int mCurline = 0; // 当前行
+
+	private int mNextLine = 1;
+
+	private int mCurLineYPos = 0;
+
+	private int mPreLineYPos = 0;
+
+	private int mNextLineYPos = 0;
+
+	private int mScrollHeight = 0;
+
+	private Scroller mScroller;
+
+	private boolean mIsScrolling = false;
+	
+	private boolean mIsPlay = false;
+
+	
+	private PlayRunnable playRunnable = new PlayRunnable();
+	
+	public YluoMarqueeView(Context context, AttributeSet attrs, int defStyleAttr) {
+		super(context, attrs, defStyleAttr);
+		init();
+	}
+
+	public YluoMarqueeView(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		init();
+	}
+
+	public YluoMarqueeView(Context context) {
+		super(context);
+		init();
+	}
+
+	private void init() {
+		mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+		mPaint.setAntiAlias(true);
+		mPaint.setColor(Color.RED);
+		mPaint.setStrokeWidth(1);
+		mPaint.setTextSize(30);
+
+		mTexRect = new Rect();
+		mPaint.getTextBounds("0", 0, 1, mTexRect);
+
+		mContents = new ArrayList<MarqueeLine>();
+
+		mContents.add(makeMarqueeLine("大促","点击领取爱宠<<我是QQ>>"));
+	
+		mContents.add(makeMarqueeLine("热","夏有乔木,雅望天堂"));
+		
+		mContents.add(makeMarqueeLine("燃","我是跑马灯---哈哈"));
+		
+
+		mNextLine = (mCurline + 1) % mContents.size();
+
+		mScroller = new Scroller(getContext());
+
+		// mPaint.
+		// mPaint.getFontSpacing()
+		// TextView tx = null;
+		// tx.setEllipsize(where)
+		// Canvas canvas = null;
+		// canvas.drawt
+		// TextUtils.ellipsize(text, p, avail, where);
+	}
+	
+	private MarqueeLine makeMarqueeLine(String preWords,String contentWord) {
+		
+		MarqueeLine marqueeLine = new MarqueeLine();
+		
+		marqueeLine.preWords = preWords;
+		
+		marqueeLine.preWordColor =  Color.RED;
+		
+		marqueeLine.contentWords = contentWord;
+		
+		marqueeLine.contentWordsColor = Color.BLACK;
+		
+		Rect preRect = new Rect();
+		
+		mPaint.getTextBounds(marqueeLine.preWords, 0, marqueeLine.preWords.length()
+				, preRect);
+		
+		marqueeLine.wordDrawX = preRect.width() +marqueeLine.wordSpace;
+		
+		return marqueeLine;
+	}
+
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+
+		if (heightMode == MeasureSpec.AT_MOST) {
+			heightMeasureSpec = MeasureSpec.makeMeasureSpec(mLineHeight,
+					MeasureSpec.EXACTLY);
+		}
+
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+		mCurLineYPos = (getMeasuredHeight() + mTexRect.height()) / 2;
+
+		mPreLineYPos = mCurLineYPos - mLineHeight;
+
+		mNextLineYPos = mCurLineYPos + mLineHeight;
+
+	}
+
+	@Override
+	public void computeScroll() {
+		if (mScroller.computeScrollOffset()) {
+
+			mScrollHeight = mScroller.getCurrY();
+
+			if (mScroller.isFinished()) {
+				mCurline = (mCurline + 1) % mContents.size();
+
+				mNextLine = (mCurline + 1) % mContents.size();
+
+				mScrollHeight = 0;
+
+				mIsScrolling = false;
+			}
+			invalidate();
+
+		}
+	}
+
+	@Override
+	public void onDraw(Canvas canvas) {
+		drawLine(canvas,mCurline,mCurLineYPos);
+		
+		if(mIsScrolling) {
+			drawLine(canvas,mNextLine,mNextLineYPos);
+		}
+	}
+	
+	private void drawLine(Canvas canvas,int drawLineNo,int drawLinePos) {
+		
+		MarqueeLine marqueeLine = mContents.get(drawLineNo);
+		
+		mPaint.setColor(marqueeLine.preWordColor);
+		
+		// 画前缀
+		canvas.drawText(marqueeLine.preWords, getPaddingLeft(), drawLinePos - mScrollHeight, mPaint);
+		
+		mPaint.setColor(marqueeLine.contentWordsColor);
+		
+		if(!marqueeLine.isEllip) {
+			marqueeLine.contentWords = (String) TextUtils.ellipsize(marqueeLine.contentWords,
+					new TextPaint(mPaint),
+					getWidth() - marqueeLine.wordDrawX - getPaddingLeft() -  getPaddingRight(),
+					TextUtils.TruncateAt.END);  
+			marqueeLine.isEllip = true;
+		}
+		
+		// 画内容的
+		canvas.drawText(marqueeLine.contentWords, getPaddingLeft() + marqueeLine.wordDrawX, 
+				drawLinePos - mScrollHeight, mPaint);
+	}
+
+	private void nextLine() {
+
+		mIsScrolling = true;
+		mScroller.startScroll(0, 0, 0, mLineHeight, 300);
+
+		invalidate();
+
+	}
+
+	public void startPlay() {
+		
+		if(mIsPlay) {
+			return;
+		}
+		mIsPlay = true;
+		post(playRunnable);
+	
+	}
+	
+	public void stopPlay() {
+		mIsPlay = false;
+	}
+	
+	public class MarqueeLine {
+		public String preWords;
+		public int preWordColor;
+		public int wordSpace = 10;
+		public String contentWords;
+		public int contentWordsColor;
+		public float wordDrawX;
+		public boolean isEllip = false;
+	}
+	
+	class PlayRunnable implements Runnable {
+
+		@Override
+		public void run() {
+			
+			if(mIsPlay) {
+				nextLine();
+				postDelayed(this, 1000);
+			}
+			
+		}
+		
+	}
+
+}
